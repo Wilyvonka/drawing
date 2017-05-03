c750ba0a1b8b8260d308a66687d2295cfe240082
Wilyvonka <viljargjerde@hotmail.com>
2017-05-03 20:51:56 +0200

    Add files via upload

---
+++ Numberphile/sketch.js
@@ -0,0 +1,112 @@
+var drawingDots = [];
+var baseDots = [];
+var checkbox;
+var UPFInput;
+var UPF;
+var FRInput;
+var FR = undefined;
+var alphaInput;
+var alphaCol;
+
+function setup() {
+    createCanvas(800, 800);
+    background(51);
+    drawingDots.push(new DrawingDot);
+    checkbox = createCheckbox('Toggle run', false);
+    checkbox.changed(toggleLoop);
+    UPF = createInput('1');
+    UPF.input(UPFChanged);
+    FRInput = createInput('60');
+    FRInput.input(FRChanged);
+    alphaInput = createSlider(0, 255, 255, 5);
+    /* for (var i = 0; i < 3; i++) {
+         baseDots.push(new BaseDot);
+     }*/
+    /* baseDots.push(new BaseDot(width / 2, 150))
+     baseDots.push(new BaseDot(width / 8, 650))
+     baseDots.push(new BaseDot(width - width / 8, 650))*/
+    frameRate(FR);
+    noLoop();
+    UPF = 1;
+}
+
+function draw() {
+    if (frameCount < 5) {
+        background(51)
+    }
+
+    alphaCol = alphaInput.value();
+
+
+    for (var s = 0; s < UPF; s++)
+        for (var i = 0; i < drawingDots.length; i++) {
+            if (baseDots[0] != undefined) {
+                drawingDots[i].render();
+                drawingDots[i].update();
+            }
+        }
+
+    for (var i = 0; i < baseDots.length; i++) {
+        baseDots[i].render();
+    }
+
+}
+
+
+
+function DrawingDot() {
+    this.pos = createVector(width - width / 8, 650);
+    this.base;
+    this.vel = createVector();
+    this.update = function () {
+        var r = floor(random(0, baseDots.length));
+        this.base = baseDots[r];
+        this.vel = p5.Vector.sub(this.base.pos, this.pos)
+        this.vel.mult(0.5)
+        this.pos.add(this.vel);
+    }
+    this.render = function () {
+        var colr = random(255);
+        var colg = random(255);
+        var colb = random(255);
+        stroke(colr, colg, colb, alphaCol);
+        strokeWeight(1);
+        point(this.pos.x, this.pos.y);
+    }
+}
+
+
+function BaseDot(x, y) {
+    this.pos = createVector(x, y);
+
+    this.render = function () {
+        stroke(0, 255, 0);
+        strokeWeight(3);
+        point(this.pos.x, this.pos.y);
+    }
+}
+
+function mousePressed() {
+    if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < width) {
+        baseDots.push(new BaseDot(mouseX, mouseY));
+        for (var i = 0; i < baseDots.length; i++) {
+            baseDots[i].render();
+        }
+    }
+}
+
+function toggleLoop() {
+    if (this.checked) {
+        loop();
+    } else {
+        noLoop();
+    }
+}
+
+function UPFChanged() {
+    UPF = this.value();
+}
+
+function FRChanged() {
+    FR = this.value();
+}
